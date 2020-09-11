b7cd8da8aa6034be7cb3584feeec73e3fd9dc576
Reinaldo Barriga Bauden <63665882+RBarrigaB@users.noreply.github.com>
2020-09-11 13:18:21 -0300

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,76 @@
+function validar() {
+    var nombre;
+    var apellidos;
+    var correo;
+    var usuario;
+    var clave;
+    var telefono;
+
+    nombre = document.getElementById("nombre").value;
+    apellidos = document.getElementById("apellidos").value;
+    correo = document.getElementById("correo").value;
+    usuario = document.getElementById("usuario").value;
+    clave = document.getElementById("pass").value;
+    telefono = document.getElementById("telefono").value;
+
+    if (nombre === "") {
+        alert("Por favor, debe ingresar un nombre");
+        return false;
+    }
+
+    else if (nombre.length > 30) {
+        alert("El nombre es muy largo");
+        return false;
+    }
+
+    else if (/\d/.test(nombre)) {
+        alert("Por favor, ingrese un nombre que contenga solamente letras");
+        return false;
+    }
+
+    if (apellidos === "") {
+        alert("Por favor, debe ingresar sus apellidos");
+        return false;
+    }
+
+    else if (apellidos.length > 80) {
+        alert("Los apellidos son demasiado largos");
+        return false;
+    }
+
+    else if (/\d/.test(apellidos)) {
+        alert("Por favor, ingrese apellidos que contengan solamente letras");
+        return false;
+    }
+
+    if (correo === "") {
+        alert("Debe ingresar un correo");
+        return false;
+    }
+    else if (correo.length > 100) {
+        alert("El correo es demasiado largo");
+    }
+
+    if (!correo.includes("@")) {
+
+        alert("Correo inválido. Ingrese un @ en la dirección de correo");
+    }
+
+    if (!correo.includes(".")) {
+        alert(" El correo " + correo + " es inválido." + "Ingrese un punto después de @")
+    }
+
+    if (usuario.length > 20) {
+        alert("Ingrese un usuario con menos de 20 caracteres")
+    }
+
+    if (telefono.length > 20) {
+        alert("Número de teléfono demasiado largo")
+    }
+    else if (!(/^\d+$/.test(telefono))) {
+        alert("Por favor, ingrese un número de teléfono en formato '91111111'");
+        return false;
+    }
+
+    alert("Registro realizado correctamente !" + "\n\n" + "Gracias por registrarte: " + nombre +" "+ apellidos + "\n\n" + "Tu usuario es: " + usuario +"\n"+ "Tu contraseña es: " + clave);
+}
